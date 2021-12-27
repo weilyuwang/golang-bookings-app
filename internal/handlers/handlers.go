07840ec71559e903a7bcc9cbe70558d46695034a
weilyuwang <weilyuwang@gmail.com>
2021-12-27 14:18:09 -0500

    insert room restriction

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -123,15 +123,18 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	startDate, err := time.Parse(layout, sd)
 	if err != nil {
 		helpers.ServerError(w, err)
+		return
 	}
 	endDate, err := time.Parse(layout, ed)
 	if err != nil {
 		helpers.ServerError(w, err)
+		return
 	}
 
 	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
 	if err != nil {
 		helpers.ServerError(w, err)
+		return
 	}
 
 	reservation := models.Reservation{
@@ -160,17 +163,33 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		})
 		if err != nil {
 			helpers.ServerError(w, err)
+			return
 		}
 		return
 	}
 
-	err = m.DB.InsertReservation(reservation)
+	newReservationID, err := m.DB.InsertReservation(reservation)
+	if err != nil {
+		helpers.ServerError(w, err)
+		return
+	}
+
+	roomRestriction := models.RoomRestriction{
+		StartDate:     startDate,
+		EndDate:       endDate,
+		RoomID:        roomID,
+		ReservationID: newReservationID,
+		RestrictionID: 1,
+	}
+
+	err = m.DB.InsertRoomRestriction(roomRestriction)
 	if err != nil {
 		helpers.ServerError(w, err)
 		return
 	}
 
 	m.App.Session.Put(r.Context(), "reservation", reservation)
+
 	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
 }
 
