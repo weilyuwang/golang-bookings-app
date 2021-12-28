79c21d87aeba259fe404f316789f048863c0904f
weilyuwang <weilyuwang@gmail.com>
2021-12-28 15:22:23 -0500

    add migrations to seed rooms and restrictions table

---
+++ migrations/20211228201854_seed_rooms_tables.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.rooms (id, room_name, created_at, updated_at) VALUES (1, 'General''s Quarters', '2021-12-26 22:48:03.000000', '2021-12-26 22:48:08.000000');
+INSERT INTO public.rooms (id, room_name, created_at, updated_at) VALUES (2, 'Major''s Suite', '2021-12-27 16:26:12.000000', '2021-12-27 16:26:16.000000');
---
+++ migrations/20211228202029_seed_restrictions_tables.postgres.up.sql
@@ -0,0 +1,2 @@
+INSERT INTO public.restrictions (id, restriction_name, created_at, updated_at) VALUES (1, 'Reservation', '2021-12-26 23:15:53.000000', '2021-12-27 23:16:05.000000');
+INSERT INTO public.restrictions (id, restriction_name, created_at, updated_at) VALUES (2, 'Owner Block', '2021-12-27 15:13:12.000000', '2021-12-27 15:13:15.000000');
