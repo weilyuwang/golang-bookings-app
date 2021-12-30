d6c3abf5955f63b0470b53c1e353e460cd573bca
weilyuwang <weilyuwang@gmail.com>
2021-12-30 15:59:45 -0500

    login and logout

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -442,3 +442,12 @@ func (m *Repository) Login(w http.ResponseWriter, r *http.Request) {
 	m.App.Session.Put(r.Context(), "flash", "Logged in successfully")
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
+
+// Logout logs out the user
+func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
+	_ = m.App.Session.Destroy(r.Context())
+
+	_ = m.App.Session.RenewToken(r.Context()) // Good Practice!
+
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+}
