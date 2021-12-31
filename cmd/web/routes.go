ae56aa50b5a35865fd758b3cf22a7e7b99429f22
weilyuwang <weilyuwang@gmail.com>
2021-12-31 17:26:15 -0500

    reservation calendar template

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -49,6 +49,7 @@ func routes(app *config.AppConfig) http.Handler {
 		mux.Post("/reservations/{src}/{id}", handlers.Repo.AdminPostShowReservation)
 		mux.Get("/process-reservation/{src}/{id}", handlers.Repo.AdminProcessReservation)
 		mux.Get("/delete-reservation/{src}/{id}", handlers.Repo.AdminDeleteReservation)
+		mux.Get("/reservations-calendar", handlers.Repo.AdminReservationCalender)
 	})
 
 	return mux
